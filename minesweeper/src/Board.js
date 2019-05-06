716a2071cb53216ec22604f332f0d70b27c8a4ee
Kyle-Ward <KyleWard38@gmail.com>
2019-05-06 13:37:16 -0500

    cancelled minesweeper project

--- minesweeper/src/Board.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-class Board extends React.Component {
-  
-}
-
-export default Board;
--- minesweeper/src/Minesweeper.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
-import Game from './Game';
-
-ReactDOM.render(<Game />, document.getElementById('root'));
-
-serviceWorker.unregister();
