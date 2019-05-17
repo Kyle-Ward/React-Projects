ddd001627aac275528094d2181f7b250814f47f9
Kyle-Ward <KyleWard38@gmail.com>
2019-05-17 09:15:00 -0500

    fixed background image

--- charity-finder/src/components/Splash/Splash.js
+++ charity-finder/src/components/Splash/Splash.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./Splash.css";
-import field from "./field.jpg";
 import Search from "../Search/Search";
 
 function Splash({ onSearchChange, onSearchSubmit }) {
   return (
     <div className="splash">
-      <img src={field} alt="field" className="image" />
       <div className="splashText">
         This app is here to help you find charities to support.
         <br />
