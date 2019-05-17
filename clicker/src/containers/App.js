49602565f67556a3739a5af6153afee2912d9570
Kyle-Ward <KyleWard38@gmail.com>
2019-05-17 16:38:59 -0500

    refactored to use hooks instead of class state

--- clicker/src/containers/App.js
+++ clicker/src/containers/App.js
@@ -1,41 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Count from "../components/Count/Count";
 import Add from "../components/Add/Add";
 import Sub from "../components/Sub/Sub";
 import Reset from "../components/Reset/Reset";
 
-const initialState = {
-  count: 0
-};
+function App() {
+  const [count, setCount] = useState(0);
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = initialState;
-  }
-
-  onAdd = () => {
-    this.setState({ count: this.state.count + 1 });
+  const onAdd = () => {
+    setCount(count + 1);
   };
 
-  onSub = () => {
-    this.setState({ count: this.state.count - 1 });
+  const onSub = () => {
+    setCount(count - 1);
   };
 
-  onReset = () => {
-    this.setState({ count: 0 });
+  const onReset = () => {
+    setCount(0);
   };
 
-  render() {
     return (
       <div>
-        <Count counter={this.state.count} />
-        <Add onAdd={this.onAdd} />
-        <Sub onSub={this.onSub} />
-        <Reset onReset={this.onReset} />
+        <Count counter={count} />
+        <Add onAdd={onAdd} />
+        <Sub onSub={onSub} />
+        <Reset onReset={onReset} />
       </div>
     );
   }
-}
 
 export default App;
