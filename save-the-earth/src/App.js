3ad58f53b62f98732f9f577913dcd6992b61011c
Kyle-Ward <KyleWard38@gmail.com>
2019-05-17 16:21:55 -0500

    added redux

--- save-the-earth/src/App.js
+++ save-the-earth/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Save the Earth!</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.props.message}</h1>
+      </div>
+    );
+  }
 }
+
+App.propTypes = {
+  message: PropTypes.string.isRequired
+};
 
 export default App;
---
+++ save-the-earth/src/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+  message: `Is this thing on?`
+}
+
+function reducer(state = initialState) {
+  return state;
+}
+
+export default reducer;
