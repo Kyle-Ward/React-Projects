cbf16122b8adb1b1a864ce25ea582e1531daf233
Kyle-Ward <KyleWard38@gmail.com>
2019-05-28 14:50:40 -0500

    added api info to gitignore

--- instant-message/src/index.js
+++ instant-message/src/index.js
@@ -6,6 +6,7 @@ import { Route, BrowserRouter as Router } from "react-router-dom";
 import LoginComponent from "./login/login";
 import SignupComponent from "./signup/signup";
 import DashboardComponent from "./dashboard/dashboard";
+import routing from "./routing";
 
 const firebase = require("firebase");
 require("firebase/firestore");
@@ -19,16 +20,6 @@ firebase.initializeApp({
   messagingSenderId: "636377994668",
   appId: "1:636377994668:web:b55cb395a9d6d1b7"
 });
-
-const routing = (
-  <Router>
-    <div id="routing-container">
-      <Route path="/login" component={LoginComponent} />
-      <Route path="/signup" component={SignupComponent} />
-      <Route path="/dashboard" component={DashboardComponent} />
-    </div>
-  </Router>
-);
 
 ReactDOM.render(routing, document.getElementById("root"));
 
