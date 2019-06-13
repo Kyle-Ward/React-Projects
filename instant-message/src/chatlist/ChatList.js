081a6875e78b5d637d075321aa73714263711cc2
Kyle-Ward <KyleWard38@gmail.com>
2019-06-12 20:22:54 -0500

    refactored chatlist component from class to function

--- instant-message/src/chatlist/ChatList.js
+++ instant-message/src/chatlist/ChatList.js
@@ -12,101 +12,88 @@ import Button from "@material-ui/core/Button";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import NotificationImportant from "@material-ui/icons/NotificationImportant";
 
-class ChatListComponent extends React.Component {
-  render() {
-    const { classes } = this.props;
+const ChatListComponent = props => {
+  const { classes } = props;
 
-    if (this.props.chats.length > 0) {
-      return (
-        <main className={classes.root}>
-          <Button
-            variant="contained"
-            fullWidth
-            color="primary"
-            className={classes.newChatBtn}
-            onClick={this.newChat}
-          >
-            New Message
-          </Button>
-          <List>
-            {this.props.chats.map((_chat, _index) => {
-              return (
-                <div key={_index}>
-                  <ListItem
-                    onClick={() => this.selectChat(_index)}
-                    className={classes.listItem}
-                    selected={this.props.selectedChatIndex === _index}
-                    alignItems="flex-start"
-                  >
-                    <ListItemAvatar>
-                      <Avatar alt="Remy Sharp">
-                        {
-                          _chat.users
-                            .filter(_user => _user !== this.props.userEmail)[0]
-                            .split("")[0]
-                        }
-                      </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText
-                      primary={
-                        _chat.users.filter(
-                          _user => _user !== this.props.userEmail
-                        )[0]
+  const userIsSender = chat =>
+    chat.messages[chat.messages.length - 1].sender === props.userEmail;
+
+  if (props.chats.length > 0) {
+    return (
+      <main className={classes.root}>
+        <Button
+          variant="contained"
+          fullWidth
+          color="primary"
+          className={classes.newChatBtn}
+          onClick={props.newChatBtnFn}
+        >
+          New Message
+        </Button>
+        <List>
+          {props.chats.map((_chat, _index) => {
+            return (
+              <div key={_index}>
+                <ListItem
+                  onClick={() => props.selectChatFn(_index)}
+                  className={classes.listItem}
+                  selected={props.selectedChatIndex === _index}
+                  alignItems="flex-start"
+                >
+                  <ListItemAvatar>
+                    <Avatar alt="Remy Sharp">
+                      {
+                        _chat.users
+                          .filter(_user => _user !== props.userEmail)[0]
+                          .split("")[0]
                       }
-                      secondary={
-                        <React.Fragment>
-                          <Typography component="span" color="textPrimary">
-                            {_chat.messages[
-                              _chat.messages.length - 1
-                            ].message.substring(0, 30)}
-                          </Typography>
-                        </React.Fragment>
-                      }
-                    />
-                    {_chat.receiverHasRead === false &&
-                    !this.userIsSender(_chat) ? (
-                      <ListItemIcon>
-                        <NotificationImportant
-                          className={classes.unreadMessage}
-                        />
-                      </ListItemIcon>
-                    ) : null}
-                  </ListItem>
-                  <Divider />
-                </div>
-              );
-            })}
-          </List>
-        </main>
-      );
-    } else {
-      return (
-        <main className={classes.root}>
-          <Button
-            variant="contained"
-            fullWidth
-            color="primary"
-            onClick={this.newChat}
-            className={classes.newChatBtn}
-          >
-            New Message
-          </Button>
-          <List />
-        </main>
-      );
-    }
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={
+                      _chat.users.filter(_user => _user !== props.userEmail)[0]
+                    }
+                    secondary={
+                      <React.Fragment>
+                        <Typography component="span" color="textPrimary">
+                          {_chat.messages[
+                            _chat.messages.length - 1
+                          ].message.substring(0, 30)}
+                        </Typography>
+                      </React.Fragment>
+                    }
+                  />
+                  {_chat.receiverHasRead === false && !userIsSender(_chat) ? (
+                    <ListItemIcon>
+                      <NotificationImportant
+                        className={classes.unreadMessage}
+                      />
+                    </ListItemIcon>
+                  ) : null}
+                </ListItem>
+                <Divider />
+              </div>
+            );
+          })}
+        </List>
+      </main>
+    );
+  } else {
+    return (
+      <main className={classes.root}>
+        <Button
+          variant="contained"
+          fullWidth
+          color="primary"
+          onClick={props.newChatBtnFn}
+          className={classes.newChatBtn}
+        >
+          New Message
+        </Button>
+        <List />
+      </main>
+    );
   }
-
-  newChat = () => {
-    this.props.newChatBtnFn();
-  };
-
-  selectChat = index => {
-    this.props.selectChatFn(index);
-  };
-
-  userIsSender = chat =>
-    chat.messages[chat.messages.length - 1].sender === this.props.userEmail;
-}
+};
 
 export default withStyles(styles)(ChatListComponent);
